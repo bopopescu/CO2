5286db8d191eea298e64c7a7cbcd04daa331f3b4
Nicolas Perraut <n.perraut@gmail.com>
2015-01-29 12:37:58 +0100

    Correction bug de configuration.
    L'adresse du serveur était 127.0.0.1 par défaut, le serveur était inaccessible de l'extérieur.
    
    Nouvelle IP par défaut : 192.168.1.61:3737 (cf pvServerMain.py)

--- pvServerMain.py
+++ pvServerMain.py
@@ -28,6 +28,7 @@ def main():
     # Prepare the framework, with iPOPO and the shell console
     # Warning: we only use the first argument of this method, a list of bundles
     logging.info("Preparing framework")
+    print("Preparing framework")
     framework = pelix.framework.create_framework((
         "pelix.ipopo.core",  # iPOPO
         "pelix.shell.core",  # Shell core (engine)  # "pelix.shell.console",# Text console
@@ -37,6 +38,7 @@ def main():
 
     # Start the framework, and the pre-installed bundles
     logging.info("Starting framework")
+    print("Starting framework")
     framework.start()
 
     # Get the bundle context of the framework, i.e. the link between the
@@ -51,13 +53,15 @@ def main():
         ipopo.instantiate('pelix.http.service.basic.factory',
                           'http-server',
                           {
-                              'pelix.http.address': '127.0.0.1',
+                              'pelix.http.address': '192.168.1.61',
                               'pelix.http.port': 3737
                           })
 
     logging.info("Installing PV bundle")
+    print("Installing PV bundle")
     context.install_bundle("pvPanelsServlet").start()
     logging.info("PV bundle installed")
+    print("PV bundle installed")
 
     # Wait for the framework to stop
     framework.wait_for_stop()
@@ -67,4 +71,5 @@ if __name__ == "__main__":
     FORMAT = '%(asctime)s  %(message)s'
     logging.basicConfig(level=logging.DEBUG, filename='pvService.log', format=FORMAT)
     logging.info('Launching main')
+    print('Launching main')
     main()
